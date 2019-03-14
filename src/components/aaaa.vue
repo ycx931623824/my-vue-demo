b0b9e1c10dba24be4469556b5e38ebb44db2ead5
熊一乐 <931623824@qqq.com>
2019-03-14 20:09:46 +0800

    m2

---
+++ src/components/aaaa.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="hello">
+    <!-- 输出变量 -->
+    <h1>{{ ms123123123g }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docss123123gitdkasadasdasshjdakjhsghdkalsjkgdjl
+        </a>1
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forumasdasdadas das das das 13123123
+        </a>
+      </li>123213123
+      <li>
+        <a213213
+          href="https://chat.vuejs.org"s
+          target="_blank"
+        >
+          Community Chatasdadasdasdasdasdasadasdasasdasdasdasdasdasdasdas
+        </a><dt></dt>
+      </li>1231231
+      <li>3123
+        <a
+          href="https://twitter.com/vuejs"2312312312312
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://122323233123vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vu123123e-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+         我的世界充满爱
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    // 定义页面名称，可以不要
+  name: 'HelloWorld',
+  data () {
+    return {
+       // 定义变量
+      msg: 'Welcome to Your 123dasdasasasa123123.jass asdassadas'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
