2020ab4c6c0fc1bfa2bc98d5d1b3f63997355758
熊一乐 <931623824@qqq.com>
2019-03-28 15:42:31 +0800

    new

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,98 +1,26 @@
 <template>
   <div class="hello">
     <!-- 输出变量 -->
-    <h1>{{ msg }}</h1>
+    <h1>父组件</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docssdkashjdakjhsghdkalsjkgdjl
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forumasdasdadas das das das 13123123
-        </a>
-      </li>123213123
-      <li>
-        <a213213
-          href="https://chat.vuejs.org"s
-          target="_blank"
-        >
-          Community Chatasdadasdasadasdasdasdasdasdasdasdas
-        </a><dt></dt>
-      </li>1231231
-      <li>3123
-        <a
-          href="https://twitter.com/vuejs"2312312312312
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-         我的世界充满爱
-        </a>
-      </li>
-    </ul>
+    <aaa></aaa>
   </div>
 </template>
 
 <script>
+import aaa from './aaa'
 export default {
     // 定义页面名称，可以不要
   name: 'HelloWorld',
+  components: {
+    aaa
+  },
   data () {
     return {
-       // 定义变量
-      msg: 'Welcome to Your 123dasdasasasa123123.jass asdassadas'
     }
+  },
+  mounted () {
+    console.log(getSelection)
   }
 }
 </script>
